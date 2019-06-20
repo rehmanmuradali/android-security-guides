490ca9f66e5027da5eb0c655b2de63515117e1c6
Rehman <rehman.muradali@venturedive.com>
2019-06-20 23:23:28 +0500

    Custom Permission Added

---
+++ app/src/main/java/com/android/androidsecurity/view/LoginActivity.kt
@@ -0,0 +1,23 @@
+package com.android.androidsecurity.view
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+import com.android.androidsecurity.R
+
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+}
